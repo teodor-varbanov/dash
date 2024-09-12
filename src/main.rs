f57356cf965bdb778b8713435b6a3558be83325b
Teodor Varbanov <tvarbanov@outlook.com>
2024-09-12 22:58:40 +0300

    not working cmd execution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use nix::sys::wait;
+use nix::unistd::{fork, ForkResult};
+use std::ffi::CString;
 use std::io;
 use std::io::Write;
 
@@ -8,5 +11,21 @@ fn main() {
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
+        let command = CString::new(input).expect("Failed to convert to CString");
+
+        match unsafe { fork() } {
+            Ok(ForkResult::Parent { child: _ }) => {
+                wait::wait().expect("Couldn't wait for some reason");
+            }
+            Ok(ForkResult::Child) => {
+                let arg_test = CString::new("-a").expect("Failed to convert to CString");
+                let path_test = CString::new("/usr/bin/").expect("Failed to convert to CString");
+                let argument = vec![arg_test];
+                let path = vec![path_test];
+
+                nix::unistd::execve(&command, &path, &argument).expect("Command not found");
+            }
+            Err(_) => println!("Fork failed"),
+        }
     }
 }
