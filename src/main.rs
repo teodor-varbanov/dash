acfb0e7a10b6ce480b24cdb9a149dd025ae85ab7
Teodor Varbanov <tvarbanov@outlook.com>
2024-09-13 16:49:56 +0300

    fixed a bracket, I think..

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use nix::sys::wait;
 use nix::unistd::{fork, ForkResult};
 use std::io;
 use std::io::Write;
-use std::process;
 use std::process::Command;
+use sysinfo::Pid;
 
 fn main() {
     loop {
@@ -12,18 +12,20 @@ fn main() {
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        println!("before match: {}", process::id());
+
+        let _pid = Pid::from_u32(0);
+
         match unsafe { fork() } {
-            Ok(ForkResult::Parent { child: _ }) => {
-                println!("parent: {}", process::id());
-                wait::wait().expect("Couldn't wait for some reason");
-                continue
+            
+            Ok(ForkResult::Parent { child: _pid }) => {
+
+               wait::wait().expect("Couldn't wait for some reason");
+
             }
             Ok(ForkResult::Child) => {
                 input.pop();
                 let child = Command::new(input).spawn();
                 let _output = child.expect("Error").wait_with_output();
-                println!("child {}", process::id());
             }
 
             Err(_) => println!("Fork failed"),
