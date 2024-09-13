cdb3454b275b4427f818363f44224131cc5a2b07
Teodor Varbanov <tvarbanov@outlook.com>
2024-09-13 11:01:26 +0300

    version with .spawn()

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,28 @@
-use nix::sys::wait;
-use nix::unistd::{fork, ForkResult};
+// use nix::sys::wait;
+// use nix::unistd::{fork, ForkResult};
 use std::io;
 use std::io::Write;
-use std::process;
+// use std::process;
 use std::process::Command;
 
 fn main() {
-    loop {
+    loop{
         print!("dash-0.1$ ");
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+        io::stdin()
+            .read_line(&mut input)
+            .unwrap();
+        input.pop();
 
-        match unsafe { fork() } {
-            Ok(ForkResult::Parent { child: _ }) => {
-                wait::wait().expect("Couldn't wait for some reason");
-            }
-            Ok(ForkResult::Child) => {
-                input.pop();
+        let command = &input;
 
-                Command::new(input)
-                    .spawn()
-                    .expect("Command not found.");
-                process::exit(0x0100);
-            }
-            
-            Err(_) => println!("Fork failed"),
+        {
+        Command::new(command)
+            .spawn()
+            .expect("Command not found.");
+        io::stdout().flush().unwrap();
         }
     }
 }
