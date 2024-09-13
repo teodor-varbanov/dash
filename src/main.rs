fcb1335f0da80ca64482b8ab6b1e53b735eb7631
Teodor Varbanov <tvarbanov@outlook.com>
2024-09-13 18:58:20 +0300

    added support for arguments

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,29 @@ use sysinfo::Pid;
 
 fn main() {
     loop {
+        //printing the terminal cursor
         print!("dash-0.1$ ");
         io::stdout().flush().unwrap();
 
+        // handling input and parsing
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
+        input.pop();
 
+        let command = input.split(" ");
+        let cmd_vec: Vec<&str> = command.collect();
+
+        // PID integer
         let _pid = Pid::from_u32(0);
 
         match unsafe { fork() } {
-            
             Ok(ForkResult::Parent { child: _pid }) => {
-
-               wait::wait().expect("Couldn't wait for some reason");
-
+                wait::wait().expect("Couldn't wait for some reason");
+                continue
             }
             Ok(ForkResult::Child) => {
-                input.pop();
-                let child = Command::new(input).spawn();
+                let child = Command::new(cmd_vec[0]).args(&cmd_vec[1..]).spawn();
+
                 let _output = child.expect("Error").wait_with_output();
             }
 
