f55086a260583e25a99abaa5be412bb8c21b2dd8
Teodor Varbanov <tvarbanov@outlook.com>
2024-09-13 15:30:53 +0300

    minor changes before additional tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use nix::sys::wait;
 use nix::unistd::{fork, ForkResult};
 use std::io;
 use std::io::Write;
-// use std::process;
+use std::process;
 use std::process::Command;
 
 fn main() {
@@ -12,15 +12,18 @@ fn main() {
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-
+        println!("before match: {}", process::id());
         match unsafe { fork() } {
             Ok(ForkResult::Parent { child: _ }) => {
+                println!("parent: {}", process::id());
                 wait::wait().expect("Couldn't wait for some reason");
+                continue
             }
             Ok(ForkResult::Child) => {
                 input.pop();
                 let child = Command::new(input).spawn();
-                let output = child.expect("Error").wait_with_output();
+                let _output = child.expect("Error").wait_with_output();
+                println!("child {}", process::id());
             }
 
             Err(_) => println!("Fork failed"),
